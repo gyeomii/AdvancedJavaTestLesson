afb04c6eff03184512953e1fe36ef83b7c8dd2cc
gyeomii <kimsg7388@gmail.com>
2022-05-19 17:29:49 +0900

    Generic

--- AdvancedJavaTestLesson/src/GenericEnumTest/T02GenericTest.java
+++ AdvancedJavaTestLesson/src/GenericEnumTest/T02GenericTest.java
@@ -24,6 +24,54 @@ V => Value
 E => Element  
   
 */
-public class T02GenericTest {
+class NonGenericClass{
+	private Object val;
 
+	public Object getVal() {
+		return val;
+	}
+
+	public void setVal(Object val) {
+		this.val = val;
+	}
+}
+
+class MyGeneric<T>{
+	private T val;
+
+	public T getVal() {
+		return val;
+	}
+
+	public void setVal(T val) {
+		this.val = val;
+	}
+}
+public class T02GenericTest {
+	public static void main(String[] args) {
+		NonGenericClass ng1 = new NonGenericClass();
+		ng1.setVal("가나다라");
+		
+		NonGenericClass ng2 = new NonGenericClass();
+		ng2.setVal(100);
+		
+		String rtnVal1 = (String) ng1.getVal();
+		System.out.println("문자열 반환값 : " + rtnVal1);
+		
+		Integer irtnVal2 = (Integer) ng2.getVal();
+		System.out.println("정수 반환값 : " + irtnVal2);
+		System.out.println();
+		MyGeneric<String> mg1 = new MyGeneric<String>();
+		MyGeneric<Integer> mg2 = new MyGeneric<Integer>();
+		
+		mg1.setVal("우리나라");
+		mg2.setVal(500);
+		
+		rtnVal1 = mg1.getVal();
+		irtnVal2 = mg2.getVal();
+		
+		System.out.println("제너릭 문자열 반환 값 : " + rtnVal1);
+		System.out.println("제너릭 정수형 반환 값 : " + irtnVal2);
+		
+	}
 }
