4933a89f1732c309e3d6e1f4c7e343689b8bd5dd
gyeomii <kimsg7388@gmail.com>
2022-05-19 17:10:02 +0900

    Generic

---
+++ AdvancedJavaTestLesson/src/GenericEnumTest/T02GenericTest.java
@@ -0,0 +1,29 @@
+package GenericEnumTest;
+
+/*
+제너릭 클래스 만드는 방법
+   
+형식) 
+	class 클래스명<제너릭타입글자> {
+		제너릭타입글자 변수명;  // 변수 선언에 제너릭을 사용할 경우
+      		...
+     
+     	제너릭타입글자 메서드명() { // 반환값이 있는 메서드에서 사용
+        	...
+        
+     		return 값;
+     	}
+  	  
+  	  		...
+  	}
+  
+-- 제너릭타입글자 --
+T => Type
+K => Key
+V => Value
+E => Element  
+  
+*/
+public class T02GenericTest {
+
+}
