4d24b48086287ee384bb01113565c870e518fd22
gyeomii <kimsg7388@gmail.com>
2022-06-02 14:50:09 +0900

    jdbc

--- AdvancedJavaTestLesson/src/JDBCTest/T02JdbcTest.java
+++ AdvancedJavaTestLesson/src/JDBCTest/T02JdbcTest.java
@@ -6,26 +6,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
-public class T01JDBCTest {
+public class T02JdbcTest {
 /*
-	JDBC를 이용한 데이터베이스 처리 순서
+	문제1) 사용자로부터 lprod_id값을 입력받아 입력한 값보다 lprod_id가 큰 자료들을 출력하시오.
 	
-	- JDBC드라이버 로딩 -> 해당 DB에 접속 -> 질의(SQL명령을 수행) -> 질의 결과 처리 -> 종료(자원반납)
-	1. JDBC로딩
-		=> JDBC드라이버는 DB를 만든 회사에서 제공한다.
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		
-	2. 접속하기 : 접속이 성공하면 Connection객체가 생성된다.
-		DriverManager.getconnection()메소드를 이용한다.
-	
-	3. 질의 : Statement객체 또는 PreparedStatement객체를 이용하여 SQL문장을 실행한다.
-	
-	4. 결과 :
-		1) SQL문이 SELECT일 경우 ResultSet객체가 만들어진다.
-			ResultSet객체에는 SELECT한 결과가 저장된다.
-		2) SQL문이 INSERT, UPDATE, DELETE일 경우 정수값을 반환한다.
-			정수값은 보통 실행에 성공한 레코드 수를 말한다.
+	문제2) lprod_id값을 2개 입력받아서 두 값 중 작은 값부터 큰 값 사이의 자료를 출력하시오
  */
 	public static void main(String[] args) {
 		//JDBC작업에 필요한 객체변수 선언
@@ -33,6 +20,18 @@ public class T01JDBCTest {
 		Statement stmt = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
+		Scanner sc = new Scanner(System.in);
+//		System.out.print("lprod_id값을 입력하세요: ");
+//		int lprodId = sc.nextInt();
+		
+		System.out.println("lprod_id를 입력하세요");
+		System.out.print("작은 수 : ");
+		int num1 = sc.nextInt();
+		System.out.print("큰 수 : ");
+		int num2 = sc.nextInt();
+		
+		int max = Math.max(num1, num2);
+		int min = Math.min(num1, num2);
 		
 		try {
 			// 1. 드라이버 로딩
@@ -43,21 +42,20 @@ public class T01JDBCTest {
 			String password = "java";
 			
 			conn = DriverManager.getConnection(url, userId, password);
-			
 			// 3. Statement 객체 생성 -> Connection객체를 이용한다.
 			stmt = conn.createStatement();
-			
 			// 4. SQL문을 Statement객체를 이용하여 실행하고 실행결과를 ResultSet객체에 저장
-			String sql = "select * from lprod"; //실행할 쿼리문
-			
+//			String sql1 = "select * from lprod where lprod_id = " + lprodId; //실행할 쿼리문
+			String sql2 = "select * from lprod where lprod_id between " + min + " and " + max ; //실행할 쿼리문
 			// SQL문이 select인 경우 executeQuery()메소드를 사용
 			// 그 외에는 executeUpdate()를 사용
-			rs = stmt.executeQuery(sql);
-			
+//			rs = stmt.executeQuery(sql1);
+			rs = stmt.executeQuery(sql2);
 			// 5. ResultSet객체에 저장되어있는 데이터를 반복문과 next()메소드를 이용하여 차례대로 읽어서 처리한다.
-			System.out.println("실행한 쿼리문 : " + sql);
+			System.out.println("===실행한 쿼리문===");
+//			System.out.println(sql1);
+			System.out.println(sql2);
 			System.out.println("===쿼리문 실행결과===");
-			
 			//rs.next() => ResultSet객체의 데이터를 가리키는 포인터를 다음 레코드로 이동시키고
 			//  		   그곳에 데이터가 있으면 true, 없으면 false를 반환한다.
 			while(rs.next()) {
@@ -67,6 +65,7 @@ public class T01JDBCTest {
 				// 2. rs.get자료형이름(컬럼번호) -> 컬럼번호는 1부터 시작
 				System.out.println("lprod_gu : " + rs.getString(2));
 				System.out.println("lprod_nm : " + rs.getString("lprod_nm"));
+				System.out.println("========================");
 			}
 			System.out.println("출력 끝");
 		}catch(SQLException e) {
