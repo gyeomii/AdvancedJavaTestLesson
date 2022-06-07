f03be605bda413eb6e8bc9c4c80894a10e183078
gyeomii <kimsg7388@gmail.com>
2022-06-07 14:58:31 +0900

    MVC

--- AdvancedJavaTestLesson/src/JavaMVCTest/member/dao/IMemberDAO.java
+++ AdvancedJavaTestLesson/src/JavaMVCTest/member/dao/IMemberDAO.java
@@ -1,9 +1,14 @@
 package JavaMVCTest.member.dao;
 
-public interface IMemberDAO {
+import JavaMVCTest.member.vo.MemberVO;
+
 /**
  * 실제 DB와 연결해서 SQL문을 수행하여 결과를 조회한 후
  * 서비스에 전달하는 DAO의 인터페이스
  * @author gyeomii
  */
+public interface IMemberDAO {
+	
+	public int insertMember(MemberVO mv);
+	
 }
