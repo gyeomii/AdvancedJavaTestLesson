13c5b88c1d7f3d59340aff4a9cb822cf6396e0b7
gyeomii <kimsg7388@gmail.com>
2022-06-09 14:05:25 +0900

    singleton

--- AdvancedJavaTestLesson/src/A10SingletonTest/MySingleton.java
+++ AdvancedJavaTestLesson/src/A10SingletonTest/MySingleton.java
@@ -8,11 +8,11 @@ public class MySingleton {
     - 싱글턴 클래스를 구성하는 방법
     
     1. 자기 자신 class의 참조변수를 멤버변수로 선언한다.
-      ( 이 변수는 private static 으로 지정한다.)
+      ( 이 변수는 private static 으로 지정한다.) //생성자를 만들지 않고 호출할 수 있도록
     
     2. 생성자를 private으로 한다.
-      (외부에서 생성자에 접근을 못하게 하기 위해서... 즉, 외부에서
-      new 명령을 사용하지 못하게 하기 위해서)
+      (외부에서 생성자에 접근을 못하게 하기 위해서... 
+      즉, 외부에서 new 명령을 사용하지 못하게 하기 위해서)
       
     3. 객체(인스턴스)는 내부에서 생성해서 이 생성된 객체를 반환하는
      메서드를 만든다.
@@ -32,7 +32,6 @@ public class MySingleton {
 		if(single == null) {
 			single = new MySingleton();
 		}
-		
 		return single;
 	}
 	
