f08cad4e79e50019855499d736e36fe5d8bd515c
gyeomii <kimsg7388@gmail.com>
2022-06-14 19:10:29 +0900

    url

--- AdvancedJavaTestLesson/src/A13NetworkTest/basic/URLConnectionTest.java
+++ AdvancedJavaTestLesson/src/A13NetworkTest/basic/URLConnectionTest.java
@@ -14,12 +14,12 @@ public class URLConnectionTest {
 	public static void main(String[] args) throws IOException {
 		// URLConnection => 어플리케이션과 URL간의 통신 연결을 위한
 		// 추상클래스
-
+		
 		// 특정 서버(예:naver서버)의 정보와 파일내용을 출력하는 예제
 		URL url = new URL("https://www.naver.com/index.html");
 
 		// Header정보 가져오기
-
+		
 		// URLConnection객체 구하기
 		URLConnection urlConn = url.openConnection();
 
@@ -27,10 +27,10 @@ public class URLConnectionTest {
 		System.out.println("Encoding : " + urlConn.getContentEncoding());
 		System.out.println("Content : " + urlConn.getContent());
 		System.out.println();
-
+		
 		// 전체 Header정보 출력하기
 		Map<String, List<String>> headerMap = urlConn.getHeaderFields();
-
+		
 		// Header의 key값 구하기
 		Iterator<String> iterator = headerMap.keySet().iterator();
 		while (iterator.hasNext()) {
@@ -40,7 +40,7 @@ public class URLConnectionTest {
 		System.out.println("-----------------------------------------");
 
 		// 해당 호스트의 페이지 내용 가져오기
-
+		
 		// 파일을 읽어오기 위한 스트림 생성하기
 		InputStream is = urlConn.getInputStream();
 		
