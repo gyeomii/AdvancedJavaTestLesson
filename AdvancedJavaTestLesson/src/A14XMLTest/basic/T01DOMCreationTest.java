238e697bd5be23464e904ecd3d9f6e6a6bfa3acb
gyeomii <kimsg7388@gmail.com>
2022-06-16 16:24:19 +0900

    XML DOM

---
+++ AdvancedJavaTestLesson/src/A14XMLTest/basic/T01DOMCreationTest.java
@@ -0,0 +1,25 @@
+package A14XMLTest.basic;
+/**
+ * XML DOM을 이용한 XML문서 생성 예제
+ * @author "Gyeom"
+ *
+ */
+public class T01DOMCreationTest {
+/*
+	W3C(World Wide Web Consortium) DOM(Document Object Model)표준
+	
+	DOM은 문서에 접근하는 표준 방법으로서, 이를 이용하면 플랫폼 및 개발언어에 독립적으로
+	문서의 내용, 구고 및 스타일 정보를 동적으로 핸들링(생성, 접근, 수정, 삭제) 할 수 있다.
+	
+	W3C DOM표준은 다음과 같이 크게 3가지로 나누어 볼 수 있다.
+	
+	Core DOM - 모든 문서타입을 위한 핵심 표준 모델(API)
+	XML DOM - XML 문서를 위한 표준 모델
+	HTML DOM - HTML 문서를 위한 표준 모델
+	
+	예를 들면, HTML DOM은 HTML엘리먼트 요소 및 속성 정보를 핸들링 하기위한 인터페이스를 제공한다.
+*/
+	public static void main(String[] args) {
+		
+	}
+}
